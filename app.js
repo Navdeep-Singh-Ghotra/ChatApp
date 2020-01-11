92937422642ea5bef0fca52a24f050f982820ce9
navdeep-singh-ghotra <NavdeepSingh.Ghotra@gmail.com>
2020-01-11 15:38:59 -0500

    CA-105 Jade view setup

--- app.js
+++ app.js
@@ -1,11 +1,27 @@
 var express = require("express");
 var app = express();
+var rooms = require("./data/rooms.json");
+
+app.set("views", "./views")
+app.set("view engine", "jade")
 
 app.use(express.static("public"));
 app.use(express.static("node_modules/bootstrap/dist"));
 
 app.get('/', function(req,res){
-    res.send('Hello World');
+    res.render("index", { 
+        title: "Home", 
+        layout: "layout"
+    });
+});
+
+app.get('/admin/rooms', function(req,res){
+    
+    res.render("rooms", { 
+        title: "Admin Rooms",
+        rooms: rooms, 
+        layout: "layout"
+    });
 });
 
 app.listen(3000, function(){
