6c2a68f0fc33460e36a1c3ace3eecd200519d357
navdeep-singh-ghotra <NavdeepSingh.Ghotra@gmail.com>
2020-01-13 22:12:23 -0500

    CA-110 API

--- app.js
+++ app.js
@@ -9,11 +9,14 @@ app.set("view engine", "jade")
 
 app.use(express.static("public"));
 app.use(express.static("node_modules/bootstrap/dist"));
+app.use(express.static("node_modules/jquery/dist"));
+
 app.use(bodyParser.urlencoded({extended: true}));
+app.use(bodyParser.json());
 
 
 app.get('/', function(req,res){
-    res.render("index", { 
+    res.render("home", { 
         title: "Home", 
         layout: "layout"
     });
@@ -21,6 +24,9 @@ app.get('/', function(req,res){
 
 var adminRouter = require("./admin");
 app.use("/admin", adminRouter);
+
+var apiRouter = require("./api")
+app.use("/api", apiRouter);
 
 app.listen(3000, function(){
     console.log('ChatApp listening on port 3000!');
